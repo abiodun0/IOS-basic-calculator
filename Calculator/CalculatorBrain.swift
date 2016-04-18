741441660afaf839b2dc49e8e401d474e84b70ba
abiodun0 <abiodun.shuaib.m@gmail.com>
2016-04-18 02:28:14 +0100

    add more functionalities

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -14,10 +14,24 @@ class CalculatorBrain {
         case Operand(Double)
         case TernaryOperator(String, (Double, Double) -> Double)
         case UnaryOperator(String, Double -> Double)
+        var description: String {
+            get {
+                switch self {
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .TernaryOperator(let op, _):
+                    return "\(op)"
+                case .UnaryOperator(let op, _):
+                    return "\(op)"
+                }
+            }
+        }
         
     }
     // var knownOps = Dictionary<String, Op>() a better way yay!
     private var knowOps = [String:Op]()
+    
+
     
     init(){
         knowOps["√"] = Op.UnaryOperator("√", sqrt)
@@ -36,6 +50,25 @@ class CalculatorBrain {
             opStack.append(operation)
         }
         return evaluate()
+    }
+    typealias someobject = AnyObject
+    var program : someobject{
+        get{
+            return opStack.map {$0.description}
+        }
+        set {
+            if let opSymbols = newValue as? Array<String> {
+                var newOpStack = [Op]()
+                for symbol in opSymbols{
+                    if let op = knowOps[symbol]{
+                        newOpStack.append(op)
+                    } else if let operand = NSNumberFormatter().numberFromString(symbol)?.doubleValue {
+                        newOpStack.append(Op.Operand(operand))
+                    }
+                }
+                opStack =  newOpStack
+            }
+        }
     }
     private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]){
         if !ops.isEmpty{
